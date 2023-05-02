e73e43d53427d74bd8634be54b5140e1fe7a2691
ConorF <49844793+psyconorlogy@users.noreply.github.com>
2023-05-02 10:22:15 +0100

    Improve performance and format

--- wordMine.sh
+++ wordMine.sh
@@ -1,43 +1,68 @@
 #!/usr/bin/python3
-
 import argparse
+import re
 from collections import Counter
 
+
+wordlist = []
+goodwords = 0
+allwords = 0
+commonEngwords = ['the', 'a', 'in','of','this','is','if','or','and','with','into','their', 'be', 'you','should', 'to', 'for','it']
+
+
 def MyCode(filename):
-
-    wordlist = []
-    goodwords = 0
-    allwords = 0
-    badchars = [',','.',':',';','!',"'",'"', '_', '#','<','>']
-    commonEngwords = ['the', 'a', 'in','of','this', 'that', 'then', 'is','if','or','and','with','into','their', 'we', 'are', 'be', 'you','should', 'to', 'for','it', 'can', 'how', 'your', 'when', 'by', 'we', 'that', 'are', 'as', 'an', 'on', 'have', 'will', 'not', 'would', 'by', 'at', 'can', 'which', 'but',  'there', 'were', 'about', 'also', 'such', 'was', 'been', 'seen', 'us']
-    
-    for line in filename.readlines():
-
-        for word in line.split():
-            allwords += 1
-            for char in word:
-                if char.isdigit() is True:
-                    continue
-                for char in badchars:
-                    word = word.replace(char,"")
-            if True in [char.isdigit() or (char in badchars) for char in word]:
-                continue
-            word = (word.rstrip().lower())
-            if word in commonEngwords:
-                continue
-            wordlist.append(word)
-            goodwords += 1
-    counts = Counter(wordlist)
+        textMine(filename)	
 
 
-    print('Allwords = ' + str(allwords) + 'total words')
-    print('This file contains: ' + str(goodwords) + ' valid words')
-    print()
-    print(counts)
+def output(filename, wordlist, goodwords, allwords):
+	print('This file contains: ' + str(goodwords) + ' valid words')
+	print('Allwords = ' +str(allwords))
+	print()
+	print(Counter(wordlist))
 
-    
+
+def scan(word):
+	global allwords, wordlist, goodwords
+	allwords+=1	
+	word = (word.rstrip().lower())
+	if word in commonEngwords:
+		return
+	wordlist.append(word)
+	goodwords+=1
+
+
+def textMine(filename):
+	### Main
+	for line in filename.readlines():
+		words = re.split(r'[\W]+', line)
+		if len(words) > 0:
+			for w in words:
+				if len(w) < 1:
+					continue
+				scan(w)
+	output(filename, wordlist, goodwords, allwords)
+
+
+def pdfMine(filename):
+        import pdfplumber
+        pdf = pdfplumber.open(filename)
+       	for page in pdf.pages:
+       		text = page.extract_text()
+       		for line in text.readlines():
+       			for word in line.split():
+       				allwords +=1
+       				if True in [char.isdigit() or (char in badchars) for char in word]:
+	       				continue
+	       			word = (word.rstrip().lower())
+	       			if word in commonEngwords:
+	       				continue
+       				wordlist.append(word)
+       				goodwords += 1
+        output(filename, wordlist, goodwords, allwords)
+
+        
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filename', type=argparse.FileType('r'))
-    args = parser.parse_args()
-    MyCode(args.filename)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('filename', type=argparse.FileType('r'))
+	args = parser.parse_args()
+	MyCode(args.filename)
